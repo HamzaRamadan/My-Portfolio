872c1f7091fff9c5126a285d8bd0220b652e3523
Ahmed AbdElgwad <Ahmed AbdElgwad@example.com>
2025-05-20 22:59:13 +0300

    Add Api

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const API_BASE_URL =  'http://localhost:5000/api'
+const API_BASE_URL =  'https://portfolio-b-production.up.railway.app/api'
 
 const api = axios.create({
   baseURL: API_BASE_URL,
